3574194cada9a97e77234463378681d8e1ac3d2f
Andreas Vifert <andreasgylche@gmail.com>
2023-02-03 18:05:45 +0100

    added formik/yup markup for login and register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import HomePage from './scenes/homePage'
 import LoginPage from './scenes/loginPage'
 import ProfilePage from './scenes/profilePage'
-import RegisterPage from './scenes/registerPage'
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile/:id" element={<ProfilePage />} />
         </Routes>
--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
+import Form from './Form'
 
 export default function LoginPage() {
-  return <div className='flex flex-col justify-center items-center w-full h-screen'>
-    <h1 className='block text-lg font-semibold mb-4'>Login</h1>
-    <form action="POST" className='flex flex-col gap-2'>
-      <label htmlFor="email">Email</label>
-      <input className='rounded border border-indigo-200' type="email" name="email" id="email" />
-      <label htmlFor="password">Password</label>
-      <input className='rounded border border-indigo-200' type="password" name="password" id="password" />
-      <input className='rounded bg-indigo-200 font-semibold py-2 px-4 mt-4' type="submit" value="Login" />
-    </form>
-  </div>
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-screen">
+      <h1 className="block text-lg font-semibold mb-4">Login</h1>
+      <Form />
+    </div>
+  )
 }
