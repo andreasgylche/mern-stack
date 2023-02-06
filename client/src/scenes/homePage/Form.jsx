6fe9941947fac33580bdef651f9d4e899c0b53a2
Andreas Vifert <andreasgylche@gmail.com>
2023-02-06 16:09:14 +0100

    added functionality to create post

---
+++ client/src/scenes/homePage/Form.jsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import Navbar from '../navbar'
+import * as yup from 'yup'
+import { Formik } from 'formik'
+import { useDispatch, useSelector } from 'react-redux'
+import { setPost } from '../../state'
+import Dropzone from 'react-dropzone'
+
+const postSchema = yup.object().shape({
+  title: yup.string().required('Field is required.'),
+  description: yup
+    .string()
+    .max(120, 'Only 120 characters allowed for description.'),
+})
+
+const initialPostValues = {
+  title: '',
+  description: '',
+}
+
+export default function Form() {
+  const dispatch = useDispatch()
+  const { _id } = useSelector((state) => state.user)
+  const token = useSelector((state) => state.token)
+
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    const formData = new FormData()
+    for (let value in values) {
+      formData.append(value, values[value])
+    }
+    formData.append('picturePath', values.picture.name)
+    formData.append('userId', _id)
+
+    console.log(1)
+
+    const savedPostResponse = await fetch('http://localhost:3001/posts', {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${token}` },
+      body: formData,
+    })
+
+    console.log(2)
+
+    const savedPost = await savedPostResponse.json()
+    onSubmitProps.resetForm()
+
+    if (savedPost) {
+      dispatch(setPost({ savedPost }))
+    }
+  }
+
+  return (
+    <Formik
+      onSubmit={handleFormSubmit}
+      initialValues={initialPostValues}
+      validationSchema={postSchema}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+        resetForm,
+      }) => (
+        <form
+          className="flex flex-col justify-center items-center w-fit"
+          onSubmit={handleSubmit}
+        >
+          <label className="self-start" htmlFor="title">
+            Post title:
+          </label>
+          <input
+            className="rounded border border-indigo-200 mb-2 text-base px-4 py-2 mt-1"
+            type="text"
+            name="title"
+            id="title"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.title}
+          />
+          {touched.title && errors.title && (
+            <p className="text-sm text-rose-500 self-start">{errors.title}</p>
+          )}
+          <label className="self-start" htmlFor="description">
+            Post description:
+          </label>
+          <textarea
+            className="rounded border border-indigo-200 mb-2 text-base px-4 py-2 mt-1"
+            name="description"
+            id="description"
+            rows="5"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.description}
+          ></textarea>
+          {touched.description && errors.description && (
+            <p className="text-sm text-rose-500 self-start">
+              {errors.description}
+            </p>
+          )}
+          <div className="rounded border border-indigo-200 p-2 w-full mt-1 mb-2">
+            <Dropzone
+              acceptedFiles=".jpg,.jpeg,.png"
+              multiple={false}
+              onDrop={(acceptedFiles) => {
+                setFieldValue('picture', acceptedFiles[0])
+              }}
+            >
+              {({ getRootProps, getInputProps }) => (
+                <div
+                  className=" rounded border border-dashed border-indigo-200 hover:cursor-pointer p-2"
+                  {...getRootProps()}
+                >
+                  <input {...getInputProps()} />
+                  {!values.picture ? (
+                    <p className="text-sm">Add picture here</p>
+                  ) : (
+                    <p className="text-sm">{values.picture.name}</p>
+                  )}
+                </div>
+              )}
+            </Dropzone>
+          </div>
+          <button
+            className="rounded bg-indigo-200 font-semibold py-2 px-4 mt-4 w-full"
+            type="submit"
+          >
+            Create post
+          </button>
+        </form>
+      )}
+    </Formik>
+  )
+}
--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,14 @@
 import Navbar from '../navbar'
+import Form from './Form'
 
 export default function HomePage() {
   return (
     <div className="w-full">
       <Navbar />
-      HomePage
+      <main className="flex flex-col items-center w-full h-screen">
+        <h1 className="block text-lg font-semibold my-4">Create a post</h1>
+        <Form />
+      </main>
     </div>
   )
 }
